762be14d56becc7e0bb634800cb59e7325f8cba0
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2025-10-20 23:37:42 +0800

    feat: allow adjacent tags

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const app = new Elysia()
       let name = "";
       // /[name]
       if (msg.startsWith("/")) {
-        const match = msg.match(/\/(\S*)\s*(.*)/s);
+        const match = msg.match(/\/([^\s#]+)\s*(.*)/s);
         if (!match) throw status(400, "invalid / command");
         [, name, msg] = match;
       }
