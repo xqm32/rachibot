fd32ecc86a1b86dddaa097de40b0a488483fcd31
Guyutongxue <guyutongxue@163.com>
2026-01-04 14:49:53 +0800

    fix: markdown style

--- src/markdown.ts
+++ src/markdown.ts
@@ -40,34 +40,34 @@ body {
   margin: 0;
 }
 .container {
-  width: 100%;
+  width: auto;
   margin: 0 auto;
   padding: 1.25rem;
 }
 @media (min-width: 640px) {
   .container {
-    max-width:640px;
+    max-width: 640px;
     padding: 3rem;
   }
 }
 @media (min-width: 768px) {
   .container {
-    max-width:768px;
+    max-width: 768px;
   }
 }
 @media (min-width: 1024px) {
   .container {
-    max-width:1024px;
+    max-width: 1024px;
   }
 }
 @media (min-width: 1280px) {
   .container {
-    max-width:1280px;
+    max-width: 1280px;
   }
 }
 @media (min-width: 1536px) {
   .container {
-    max-width:1536px;
+    max-width: 1536px;
   }
 }
 @layer components {
