4d032ff27ebf06d68db09943dfaca2010da1e06d
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2026-09-15 17:00:31 +0800

    feat: add alias for 💰💰

--- src/index.ts
+++ src/index.ts
@@ -790,8 +790,8 @@ const app = new Elysia()
           `🤑 $${(total_credits - total_usage).toFixed(2)}`,
         ].join("\n");
       }
-      // 💰💰 | ly
-      else if (msg === "💰💰" || msg === "ly") {
+      // 💰💰 | ly | yy
+      else if (msg === "💰💰" || msg === "ly" || msg === "yy") {
         const config = new Config({ credential: new Credential() });
         config.endpoint = "business.aliyuncs.com";
         const client = new BssOpenApi20171214(config);
